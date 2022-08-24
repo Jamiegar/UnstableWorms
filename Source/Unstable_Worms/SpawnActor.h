0108852278706a8efbad6ab0b8c51743f5181e89
Jamie Linnell <jamiegar99@gmail.com>
2022-08-24 16:07:13 +0100

    First commit

---
+++ Source/Unstable_Worms/SpawnActor.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SpawnActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNSTABLE_WORMS_API USpawnActor : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+
+};
